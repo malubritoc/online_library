d882e3854a3c12358f7ac2da84841db839c5ee56
Maria Luìsa <malubritoc@MacBook-Air-de-Maria.local>
2024-06-22 18:15:36 -0300

    fix: fix spelling of category titles

--- src/components/category/category-title.tsx
+++ src/components/category/category-title.tsx
@@ -1,7 +1,22 @@
 export function CategoryTitle({ title }: { title: string }) {
+  function getTitle(title: string) {
+    switch (title) {
+      case "biblias":
+        return "Bíblias";
+      case "teologia":
+        return "Teologia";
+      case "filosofia":
+        return "Filosofia";
+      case "ciencia":
+        return "Ciência";
+      case "outros":
+        return "Outras...";
+    }
+  }
+
   return (
     <div className="w-full p-14 rounded-[18px] bg-yellow-main text-5xl text-white font-bold">
-      <h1>/{title}</h1>
+      <h1>/{getTitle(title)}</h1>
     </div>
   );
 }
