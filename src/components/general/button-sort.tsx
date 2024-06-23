f91edc80b30c48e34561678f96690ec6f5a0d745
Maria Luìsa <malubritoc@MacBook-Air-de-Maria.local>
2024-06-23 09:55:05 -0300

    feat: create sort options component

--- src/components/general/button-sort.tsx
+++ src/components/general/button-sort.tsx
@@ -1,10 +1,35 @@
 import { SortIcon } from "@/assets/home/sortIcon";
+import clsx from "clsx";
+import { useState } from "react";
 
 export function SortButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <button className="flex gap-2 items-center px-3 py-2 border border-[#E1E5EA] rounded-[26px]">
-      <SortIcon />
-      <p className="text-xs">Ordenar</p>
-    </button>
+    <div className="relative">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={clsx(
+          "flex gap-2 items-center",
+          "px-3 py-2 border border-[#E1E5EA] rounded-[26px]",
+          "cursor-pointer"
+        )}
+      >
+        <SortIcon />
+        <p className="text-xs">Ordenar</p>
+      </button>
+      <div
+        data-open={isOpen}
+        className={clsx(
+          "w-full absolute top-[36px] hidden data-[open=true]:flex flex-col gap-2",
+          "px-3 py-2 border border-[#F3E7FA]",
+          "bg-gray-bg-screen shadow-custom z-20"
+        )}
+      >
+        <p className="text-[10px] py-1">Maior preço</p>
+        <div className="w-full h-0 border-b-[2px] border-b-[#F3E7FA]" />
+        <p className="text-[10px] py-1">Menor preço</p>
+      </div>
+    </div>
   );
 }
