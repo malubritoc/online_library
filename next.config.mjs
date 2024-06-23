5380fe1c15e0ee8f3f69c5bb8c8a8d8f23ca7599
Maria Luìsa <malubritoc@MacBook-Air-de-Maria.local>
2024-06-23 09:27:33 -0300

    feat redirect from '/' to '/signin'

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/signin',
+          permanent: false, 
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
